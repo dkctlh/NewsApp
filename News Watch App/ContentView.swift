e54eb39e81c61c144792a876581f463069f839a4
Talha Dikici <dikicitalha@gmail.com>
2022-11-24 19:52:07 +0300

    Initial Commit

---
+++ News Watch App/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  News Watch App
+//
+//  Created by Talha Dikici on 24.11.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ News Watch App/NewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewsApp.swift
+//  News Watch App
+//
+//  Created by Talha Dikici on 24.11.2022.
+//
+
+import SwiftUI
+
+@main
+struct News_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
